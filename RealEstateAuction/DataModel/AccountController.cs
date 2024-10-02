83d7b6cd5f64af8cef59ef22c0ef7051a89b5ab1
DangNgocThanhk18 <thanhdnhe186611@fpt.edu.vn>
2024-10-02 18:16:32 +0700

    create controller change password

---
+++ RealEstateAuction/DataModel/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateAuction.DAL;
+using RealEstateAuction.DataModel;
+using RealEstateAuction.Models;
+using System.Security.Claims;
+
+
+namespace RealEstateAuction.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserRepository userDAO;
+        private IMapper _mapper;
+
+        public AccountController(IMapper mapper)
+        {
+            userDAO = new UserRepository();
+            _mapper = mapper;
+        }
+
+        
+
+        [HttpGet]
+        [Authorize]
+        [Route("change-password")]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("change-password")]
+        public IActionResult ChangePassword(ChangePasswordModel passwordData)
+        {
+            //if value of user enter is not valid
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                //find User by Id
+                int id = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = userDAO.GetUserById(id);
+
+                //verify old password of user
+                if (!user.Password.Equals(passwordData.Password))
+                {
+                    ModelState.AddModelError("Password", "Mật khẩu cũ không đúng!");
+                    return View();
+                }
+                else
+                {
+                    //update new password
+                    userDAO.UpdatePassword(user.Email, passwordData.NewPassword);
+                    TempData["Message"] = "Thay đổi mật khẩu thành công!";
+                    return View();
+                }
+            }
+        }
+    }
+}
