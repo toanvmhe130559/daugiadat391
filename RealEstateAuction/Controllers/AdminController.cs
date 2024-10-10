083173072bff62db8d00fbe04ccee00ff813ac33
DangNgocThanhk18 <thanhdnhe186611@fpt.edu.vn>
2024-10-10 13:16:35 +0700

    Create create staff

--- RealEstateAuction/Controllers/AdminController.cs
+++ RealEstateAuction/Controllers/AdminController.cs
@@ -1,10 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using NuGet.Protocol.Plugins;
 using RealEstateAuction.DAL;
-using RealEstateAuction.DataModel;
 using RealEstateAuction.Enums;
 using RealEstateAuction.Models;
 
@@ -23,9 +20,9 @@ namespace RealEstateAuction.Controllers
             return View();
         }
 
-        
 
-       
+
+
         [Authorize(Roles = "Admin")]
         [HttpGet("manage-member")]
         public IActionResult ManageMember(int? page)
@@ -34,5 +31,50 @@ namespace RealEstateAuction.Controllers
 
             return View(userDAO.GetMember(PageNumber));
         }
+        [Authorize(Roles = "Admin")]
+        [HttpPost("create-staff")]
+        public IActionResult CreateStaff()
+        {
+            string fullName = Request.Form["fullName"];
+            string email = Request.Form["email"];
+            string pwd = Request.Form["pwd"];
+            string phone = Request.Form["phone"];
+            string date = Request.Form["dob"];
+
+            User user = new User()
+            {
+                FullName = fullName,
+                Email = email,
+                Password = pwd,
+                Phone = phone,
+                Dob = DateTime.Parse(date),
+                RoleId = (int)Roles.Staff,
+                Address = "",
+                Wallet = 0,
+                Status = (int)Status.Active,
+            };
+
+            var exist = userDAO.GetUserByEmail(email);
+            if (exist != null)
+            {
+                TempData["Message"] = "Email already exists!";
+                ViewBag.User = user;
+            }
+            else
+            {
+                var result = userDAO.AddUser(user);
+                if (result)
+                {
+                    TempData["Message"] = "Register successful!";
+                }
+                else
+                {
+                    TempData["Message"] = "Register fail!";
+                }
+            }
+
+            return RedirectToAction("ManageStaff");
+        }
+
     }
 }
